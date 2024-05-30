3ccc61da3d398f0241e524682dfc758d7e90ffdd
Mark Clutario <clutariomark@gmail.com>
2024-05-30 21:45:15 +0800

    feat: add html report

--- log_activity.py
+++ log_activity.py
@@ -1,13 +1,17 @@
 import os
+import math
 import time
 from datetime import datetime
 from pathlib import Path
 
+from jinja2 import Environment, FileSystemLoader, Template
 from PIL import Image
-from Xlib import X, display
+from Xlib import X, display, error, xobject, protocol
 
 
-def get_active_window(disp_obj: display.Display):
+def get_active_window(
+    disp_obj: display.Display,
+) -> xobject.drawable.Window | None:
     root = disp_obj.screen().root
     net_active_window: int = disp_obj.intern_atom("_NET_ACTIVE_WINDOW")
     active_window_id: int = root.get_full_property(
@@ -15,21 +19,39 @@ def get_active_window(disp_obj: display.Display):
     ).value[0]
     active_window = disp_obj.create_resource_object("window", active_window_id)
 
+    try:
+        active_window.get_wm_class()
+    except error.BadWindow:
+        active_window = None
+
     return active_window
 
 
-def get_window_properties(window) -> dict:
+def get_window_properties(window: xobject.drawable.Window) -> dict:
     class_name: str = ""
+    window_name: str = ""
+
     window_class: tuple[str, str] | None = window.get_wm_class()
     if window_class is not None:
         class_name = window_class[1]
 
-    window_name: str = window.get_full_property(
-        window.display.get_atom("WM_NAME"), X.AnyPropertyType
-    ).value.decode()
-    geometry = window.get_geometry()
+    temp_window_name: protocol.request.GetProperty | None = (
+        window.get_full_property(
+            window.display.get_atom("WM_NAME"), X.AnyPropertyType
+        )
+    )
 
-    return {
+    if temp_window_name is None:
+        temp_window_name = window.get_full_property(
+            window.display.get_atom("_NET_WM_NAME"), X.AnyPropertyType
+        )
+
+    if temp_window_name is not None:
+        window_name = temp_window_name.value.decode()
+
+    geometry: protocol.request.GetGeometry = window.get_geometry()
+
+    window_props: dict = {
         "window_class": class_name,
         "window_name": window_name,
         "x": geometry.x,
@@ -38,12 +60,17 @@ def get_window_properties(window) -> dict:
         "height": geometry.height,
     }
 
+    return window_props
+
 
 def save_screenshot(
-    window, window_props: dict, output_path: Path, datetimestr: str
+    window: xobject.drawable.Window,
+    window_props: dict,
+    output_path: Path,
+    datetimestr: str,
 ) -> str:
-    root = window.query_tree().root
-    raw = root.get_image(
+    root: xobject.drawable.Window = window.query_tree().root
+    raw: protocol.request.GetImage = root.get_image(
         window_props["x"],
         window_props["y"],
         window_props["width"],
@@ -51,7 +78,7 @@ def save_screenshot(
         X.ZPixmap,
         0xFFFFFFFF,
     )
-    image = Image.frombytes(
+    image: Image.Image = Image.frombytes(
         "RGB",
         (window_props["width"], window_props["height"]),
         raw.data,
@@ -59,7 +86,7 @@ def save_screenshot(
         "BGRX",
     )
 
-    file_name = output_path.joinpath(
+    file_name: Path = output_path.joinpath(
         f"screenshot_{datetimestr}_{window_props["window_class"]}.png"
     )
     image.save(file_name)
@@ -67,43 +94,90 @@ def save_screenshot(
     return str(file_name)
 
 
+def create_html_report(
+    window_props: dict,
+    screenshot_file_name: str,
+    outpath: Path,
+    timestamp: datetime,
+) -> str:
+    datetimestr: str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    html_log_str: str = (
+        f"<tr><td>{datetimestr}</td>"
+        + f"<td>{window_props["window_class"]}</td>"
+        + f"<td>{window_props["window_name"]}</td>"
+        + f'<td><a href="{screenshot_file_name}">screenshot</a></td></tr>'
+    )
+
+    table_file: Path = outpath.joinpath("tablecontents.html")
+    with table_file.open("a") as fw:
+        fw.write(f"{html_log_str}\n")
+
+    env: Environment = Environment(
+        loader=FileSystemLoader(["templates/", str(outpath)])
+    )
+
+    table: Template = env.get_template("table.html")
+    title_str: str = timestamp.strftime("%Y %B %d")
+    content: str = table.render(title=title_str)
+
+    datestr: str = timestamp.strftime("%Y%m%d")
+    report_file: Path = outpath.joinpath(f"report_{datestr}.html")
+    with report_file.open("w") as fw:
+        fw.write(content)
+
+    return str(report_file)
+
+
 def main() -> None:
     disp_obj: display.Display = display.Display()
 
     while True:
-        active_window = get_active_window(disp_obj)
-        active_window_props: dict = get_window_properties(active_window)
-
-        timestamp: datetime = datetime.now()
-
-        main_outpath: Path = Path(".").resolve()
-        logs_dir: str | None = os.getenv("PERSONAL_LOGS")
-        if logs_dir is not None:
-            main_outpath = Path(logs_dir)
-
-        datestr: str = timestamp.strftime("%Y%m%d")
-        day_outpath: Path = main_outpath.joinpath(datestr)
-        hour_outpath: Path = day_outpath.joinpath(timestamp.strftime("%H"))
-
-        if not hour_outpath.is_dir():
-            hour_outpath.mkdir(parents=True, exist_ok=True)
-
-        datetimestr: str = timestamp.strftime("%Y%m%d-%H%M%S")
-        screenshot_file_name: str = save_screenshot(
-            active_window, active_window_props, hour_outpath, datetimestr
+        active_window: xobject.drawable.Window | None = get_active_window(
+            disp_obj
         )
 
-        log_str: str = f"{datetimestr}: " + \
-            f"{active_window_props["window_class"]} -- {screenshot_file_name}"
+        if active_window is not None:
+            active_window_props: dict = get_window_properties(active_window)
 
-        report_file: Path = day_outpath.joinpath(f"report_{datestr}.md")
+            timestamp: datetime = datetime.now()
+            second_val: int = math.floor(timestamp.second) // 10 * 10
+            timestamp: datetime = timestamp.replace(
+                second=second_val, microsecond=0
+            )
 
-        with report_file.open("a") as fw:
-            fw.write(f"{log_str}\n")
+            main_outpath: Path = Path(".").resolve()
+            logs_dir: str | None = os.getenv("PERSONAL_LOGS")
+            if logs_dir is not None:
+                main_outpath = Path(logs_dir)
 
-        print(log_str)
+            datestr: str = timestamp.strftime("%Y%m%d")
+            day_outpath: Path = main_outpath.joinpath(datestr)
+            hour_outpath: Path = day_outpath.joinpath(timestamp.strftime("%H"))
 
-        time.sleep(10)
+            if not hour_outpath.is_dir():
+                hour_outpath.mkdir(parents=True, exist_ok=True)
+
+            datetimestr: str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+            screenshot_file_name: str = save_screenshot(
+                active_window, active_window_props, hour_outpath, datetimestr
+            )
+
+            create_html_report(
+                active_window_props,
+                screenshot_file_name,
+                day_outpath,
+                timestamp,
+            )
+
+            log_str: str = (
+                f"{datetimestr}: "
+                + f"{active_window_props["window_class"]} -- "
+                + f"{active_window_props["window_name"]}"
+            )
+
+            print(log_str)
+
+            time.sleep(10)
 
 
 if __name__ == "__main__":
